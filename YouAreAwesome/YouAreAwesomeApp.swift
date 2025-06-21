f40f674a27604b09adec1f71a74f41edc7864113
Shahzad Shahzada <shahzada.s@outlook.com>
2025-06-21 14:24:51 -0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Shahzad Shahzada on 2025-06-21.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
